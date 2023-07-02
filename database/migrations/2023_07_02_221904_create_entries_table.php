d52184dcc06ae18c88f16b8948f246b76b43d1d5
Keyeisha <whitekeyeisha@gmail.com>
2023-07-02 18:01:09 -0500

    first commit

---
+++ database/migrations/2023_07_02_221904_create_entries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('username');
+            $table->text('telephone');
+            $table->text('email');
+            $table->boolean('is_18_or_older');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
